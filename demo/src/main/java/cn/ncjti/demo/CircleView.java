da86802188ff49b4af1a6576a6e33fad8f36885b
freedomou <51912192@qq.com>
2023-03-17 11:57:45 +0800

    modify file CircleView

---
+++ demo/src/main/java/cn/ncjti/demo/CircleView.java
@@ -0,0 +1,32 @@
+package cn.ncjti.demo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CircleView extends View {
+    public CircleView(Context context) {
+        super(context);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int r = getMeasuredWidth() / 2;
+        int centerX = getLeft() + r;
+        int centerY = getTop()+ r;
+        Paint paint = new Paint();
+        paint.setColor(Color.YELLOW);
+        //开始绘制
+        canvas.drawCircle(centerX, centerY, r, paint);
+    }
+}
